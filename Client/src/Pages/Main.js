c25283471c7ff8ca751c1ea5bbe09d7679d54c7a
ProjectBYH <wnrhd45@gmail.com>
2022-05-19 15:02:15 +0900

    프로젝트 병합

--- Client/src/Pages/Main.js
+++ Client/src/Pages/Main.js
@@ -6,11 +6,7 @@ import React, { useState } from 'react';
 import Header from '../Components/Header.js';
 import Post from '../Components/Post';
 import PostUploadModal from '../Components/PostUploadModal.js';
-<<<<<<< HEAD
-// import dummyData from '../../Public/dummyData.js'
-=======
 import dummyData from '../Source/dummyData.js';
->>>>>>> db9d785857cf268728f2af6cd7c7ebc4e88ed1df
 
 const Main = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,24 +14,10 @@ const Main = () => {
   const openPostUploadModal = () => {
     setIsOpen(!isOpen)
   };
-<<<<<<< HEAD
-
-  // const [posts, newPost] = useState(dummyData);
-=======
->>>>>>> db9d785857cf268728f2af6cd7c7ebc4e88ed1df
   return (
     <>
       <Header />
       <button className="postUpload" onClick={(openPostUploadModal)}>
-<<<<<<< HEAD
-        등록하기</button>
-        {/*회원 가입 여부에 따라 업로드모달 열어주기도 넣어야..*/
-        (isOpen ? <PostUploadModal />: null)
-        }
-      {/* <div className="post_area">
-        {posts.map(el => <Post post = {el} key = {el.id} />)}
-      </div> */}
-=======
           등록하기</button>
           {/*회원 가입 여부에 따라 업로드모달 열어주기도 넣어야..*/
           (isOpen ? <PostUploadModal />: null)
@@ -45,7 +27,6 @@ const Main = () => {
           {dummyData.map(el => <Post post = {el} key = {el.id} />)}
         </div>
       </div>
->>>>>>> db9d785857cf268728f2af6cd7c7ebc4e88ed1df
     </>
   );
 };
